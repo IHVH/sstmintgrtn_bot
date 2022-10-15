b9d614cf95594ae5308554126b525198d5ec986f
BigBeatProphet <ilin.vanya00@gmail.com>
2022-10-15 10:47:43 +0300

    more of minor updates
    maked most of main funcs async, maked some updates to main cog but it's still not working

--- sid/bot.py
+++ sid/bot.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 import os
 import json
 from discord.ext import commands
@@ -17,24 +18,29 @@ Bot = commands.Bot(command_prefix=cfx['prefix'], activity=activity, intents=inte
 @Bot.command(pass_context=True)
 #@commands.is_owner()
 async def load(ctx, extension):
-    Bot.load_extension(f"cogs.{extension}")
+    await Bot.load_extension(f"cogs.{extension}")
 
 @Bot.command(pass_context=True)
 #@commands.is_owner()
 async def unload(ctx, extension):
-    Bot.unload_extension(f"cogs.{extension}")
+    await Bot.unload_extension(f"cogs.{extension}")
 
 @Bot.command(pass_context=True)
 #@commands.is_owner()
 async def reload(ctx, extension):
-    Bot.unload_extension(f"cogs.{extension}")
-    Bot.load_extension(f"cogs.{extension}")
+    await Bot.unload_extension(f"cogs.{extension}")
+    await Bot.load_extension(f"cogs.{extension}")
 
 #TODO - этим образом оно не работает, пока подгрузка cog'ов разве что руками
-'''
-for filename in os.listdir(cwd+"\cogs"):
-    if filename.endwith(".py"):
-        Bot.load_extension(f"cogs.{filename[:-3]}")
-'''
+async def auto_load_extensions():
+    for filename in os.listdir(cwd+"/cogs"):
+        if filename.endswith(".py"):
+            # cut off the .py from the file name
+            await Bot.load_extension(f"cogs.{filename[:-3]}")
 
-Bot.run(token)
+async def main():
+    async with Bot:
+        await auto_load_extensions()
+        await Bot.run(token)
+
+asyncio.run(main())
--- sid/cogs/main.py
+++ sid/cogs/main.py
@@ -1,11 +1,15 @@
 import discord
 import json
+from pathlib import Path
 from discord import Message, message
 from discord.ext import commands
 from discord.ui import Button, View
 
-class User(commands.cog):
+cwd = Path(__file__).parents[1]
+cwd = str(cwd)
+cfx = json.load(open(cwd+'\json\cfx.json', encoding='utf-8'))
 
+class User(commands.cog):
     def __init__(self, Bot):
         self.Bot = Bot
 
@@ -15,7 +19,7 @@ class User(commands.cog):
 
     @commands.command()
     async def tm(ctx, *, arg: str):
-        await ctx.send(arg, delete_after=3)
+        await ctx.send(arg)
 
-def setup(Bot):
-    Bot.add_cog(User(Bot))
+async def setup(Bot):
+    await Bot.add_cog(User(Bot))
