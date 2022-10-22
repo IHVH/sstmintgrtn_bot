9f2d06d9b87a7a6bc0ebb664236121b61314924e
BigBeatProphet <ilin.vanya00@gmail.com>
2022-10-22 11:48:47 +0300

    minor update
    reworked members to embed messages
    added colours json file for embeds

--- sid/cogs/members.py
+++ sid/cogs/members.py
@@ -2,13 +2,16 @@ import discord
 import json
 import logging
 from pathlib import Path
-from discord import Message, message
+from discord import Message, message, Color
 from discord.ext import commands
 from discord.ui import Button, View
 
 
 cfx_path = Path.cwd() / 'sid' / 'json' / 'cfx.json'
 cfx = json.load(open(cfx_path, encoding='utf-8'))
+
+colour_path = Path.cwd() / 'sid' / 'json' / 'colours.json'
+colour = json.load(open(colour_path, encoding='utf-8'))
 
 class MembersCog(commands.Cog):
     def __init__(self, Bot):
@@ -22,12 +25,30 @@ class MembersCog(commands.Cog):
     @commands.Cog.listener()
     async def on_member_joined(self, member: discord.Member):
         channel = self.Bot.get_channel(cfx['info_channel'])
-        await channel.send(f'{member.display_name} Вошел в {member.joined_at}')
+
+        embed = discord.Embed(title=f'{member.display_name} - Прибыл на сервер', description="\uFEFF", colour=Color.from_str(colour['red']))
+        embed.add_field(name="Member name", value=member.mention, inline=True)
+        embed.add_field(name="Joined at", value=member.joined_at, inline=True)
+
+        await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
         channel = self.Bot.get_channel(cfx['info_channel'])
-        await channel.send(f'{member.display_name} Вышел в {member.removed_at}')
+
+        embed = discord.Embed(title=f'{member.display_name} - Покинул сервер', description="\uFEFF", colour=Color.from_str(colour['red']))
+        embed.add_field(name="Member name", value=member.mention, inline=True)
+        embed.add_field(name="Joined at", value=member.joined_at, inline=True)
+
+        await channel.send(embed=embed)
+
+    @commands.command()
+    async def test(self, ctx, member: discord.Member):
+        
+        embed = discord.Embed(title=f'{member.display_name} - info', description="\uFEFF", colour=Color.from_str(colour['green']))
+        embed.add_field(name="Member name", value=member.mention, inline=True)
+        embed.add_field(name="Joined at", value=member.joined_at, inline=True)
+        await ctx.send(embed=embed)
 
 async def setup(Bot):
     await Bot.add_cog(MembersCog(Bot))
