47a194e5720faf5050a77febc05af63f6175dfd5
BigBeatProphet <ilin.vanya00@gmail.com>
2022-11-19 12:40:28 +0300

    small fixes

--- sid/cogs/request.py
+++ sid/cogs/request.py
@@ -19,14 +19,19 @@ class Request_Cog(commands.Cog):
         await ctx.send(req['image'])
 
     @commands.command()
-    async def grav(self, ctx, arg):
+    async def grav(self, ctx, size, arg):
+        '''
+        синтаксис: 
+        !graw <размер> <email>
+        '''
         email = libgravatar.Gravatar(arg)
-        await ctx.send(email.get_image())
-        await ctx.send(email.get_image(default='identicon', force_default=True))
-        await ctx.send(email.get_image(default='monsterid', force_default=True))
-        await ctx.send(email.get_image(default='wavatar', force_default=True))
-        await ctx.send(email.get_image(default='robohash', force_default=True))
-        await ctx.send(email.get_image(default='retro', force_default=True))
+        size = int(size)
+        await ctx.send(email.get_image(size=size))
+        await ctx.send(email.get_image(size=size, default='identicon', force_default=True))
+        await ctx.send(email.get_image(size=size, default='monsterid', force_default=True))
+        await ctx.send(email.get_image(size=size, default='wavatar', force_default=True))
+        await ctx.send(email.get_image(size=size, default='robohash', force_default=True))
+        await ctx.send(email.get_image(size=size, default='retro', force_default=True))
 
 async def setup(Bot):
     await Bot.add_cog(Request_Cog(Bot))
