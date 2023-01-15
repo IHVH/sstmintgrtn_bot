32a2a1e9986b54f78c30aec19132905708fb16b6
David Shariev <88663294+DavidShariev@users.noreply.github.com>
2023-01-15 15:41:50 +0300

    "David Shariev: GET_GIF"

---
+++ src/functions/gif.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+API = "https://api.giphy.com/v1/gifs/search"
+api_key = "CXM2Bu2wkGTnWOc6zX9PnD3qjHJVfj4Q"
+headers = {
+    "api_key": api_key,
+    "q": "",
+    "limit": 1
+}
+
+
+def main(query):
+    headers["q"] = query
+    if (headers["q"] == ""):
+        headers["q"] = "nothing"
+
+    response = requests.get(API, headers).json()
+    gif_url = response["data"][0]["images"]["original"]["url"]
+    return gif_url
