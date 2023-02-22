46519dc8304a082d5a75371580af63b4e4607b41
F1mca <114800533+F1mca@users.noreply.github.com>
2023-02-22 16:06:53 +0300

    Create mks.py
    добавлена функция /mks

---
+++ src/functions/mks.py
@@ -0,0 +1,26 @@
+import telebot
+from telebot import types
+import urllib.request
+import json
+
+tgtoken=json.load(open("token.json"))
+bot = telebot.TeleBot(tgtoken['token']);
+bot.message_handler(commands=["start"])
+
+@bot.message_handler(commands=["start"])
+def inline(message):
+ key = types.InlineKeyboardMarkup()
+ cord = types.InlineKeyboardButton(text="да", callback_data="да")
+ key.add(cord)
+ bot.send_message(message.chat.id, "хотите узнать местоположение мкс?", reply_markup=key)
+
+@bot.callback_query_handler(func=lambda c: True)
+def inline(c):
+    if c.data == 'да':
+     key = types.InlineKeyboardMarkup()
+     req = urllib.request.urlopen("http://api.open-notify.org/iss-now.json")
+     obj = json.loads(req.read())
+     bot.send_message(c.message.chat.id,f"отметка времени {obj['timestamp']}", reply_markup=key)
+     bot.send_message(c.message.chat.id, f"долгота {obj['iss_position']['longitude']} и ширина  {obj['iss_position']['latitude']}", reply_markup=key)
+
+bot.polling(none_stop=True)
