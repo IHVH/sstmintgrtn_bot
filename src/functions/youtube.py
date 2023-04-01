e6041b0f93a2527f2dd1089792656b9bd989bfc3
Aital Maximov <106739018+IronRavencrest@users.noreply.github.com>
2023-04-01 07:09:03 +0300

    Update youtube.py

--- src/functions/youtube.py
+++ src/functions/youtube.py
@@ -1,13 +1,23 @@
 import os
-from bot_func_abc import BotFunctionABC
+import telebot
 import requests
 import json
+from telebot import types
+from typing import List
+from bot_func_abc import BotFunctionABC
 
 
 class YoutubeFinder(BotFunctionABC):
-    def YTFinder(self):
+    def set_handlers(self, bot: telebot.TeleBot, commands: List[str]):
+        self.bot = bot
+
+        @bot.message_handler(commands=commands)
+        def message_handler(message: types.Message):
+            bot.send_message(text=self.YTFinder(message), chat_id=message.chat.id)
+
+    def YTFinder(self, message):
         api_key = os.environ["YTFINDERTOKEN"]
-        word = self.strip("/YTfind").lower()
+        word = message.text("/YTfind").lower()
         query = word
         url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q={query}&key={api_key}'
         response = requests.get(url)
@@ -16,3 +26,4 @@ class YoutubeFinder(BotFunctionABC):
         for item in data['items']:
             result = f"Название видео: {item['snippet']['title']}"
             return result
+
