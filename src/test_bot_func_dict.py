ff91a522144a49ca3182aa0c5e33720fd48c6653
IHVH <IHVH@inbox.ru>
2023-04-06 20:34:32 +0300

    old bot link  test  skip

--- src/test_bot_func_dict.py
+++ src/test_bot_func_dict.py
@@ -2,6 +2,7 @@ import unittest
 from bot_func_dictionary import BOT_FUNCTIONS_2, BOT_FUNCTIONS
 from bot_func_abc import BotFunctionABC
 import requests
+import time
 
 class TestTeleBot(unittest.TestCase):
     
@@ -46,14 +47,16 @@ class TestTeleBot(unittest.TestCase):
                 url = f"https://github.com/{authr}"
                 response = requests.get(url)
                 self.assertEqual(response.status_code, 200, msg=f"{bf_key} {authr} The author's name must match the github account name to be able to generate a link!")
+                time.sleep(2)
 
-    #@unittest.skip("demonstrating skipping")
+    @unittest.skip("skipping test_github_account_link_old_bot_functions")
     def test_github_account_link_old_bot_functions(self):
         for bf_key, bf_value in BOT_FUNCTIONS.items():
             for authr in bf_value.authors:
                 url = f"https://github.com/{authr}"
                 response = requests.get(url)
                 self.assertEqual(response.status_code, 200, msg=f"{bf_key} {authr} The author's name must match the github account name to be able to generate a link!")
+                time.sleep(2)
 
 if __name__ == '__main__':
     unittest.main()
