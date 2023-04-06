0e9d5fa4f739a9d46bb4d8a347907b07a8c7c635
IHVH <IHVH@inbox.ru>
2023-04-06 17:27:32 +0300

    test pylint not condition

--- src/functions/github2.py
+++ src/functions/github2.py
@@ -4,6 +4,7 @@ from telebot import types
 from telebot.callback_data import CallbackData
 from typing import List
 import requests
+import os
 
 class GitHubFunctions(BotFunctionABC):
     OWNER = 'IHVH'
