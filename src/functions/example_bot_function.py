85c796011bb516b2df867de3c76ed923eb4f497c
IHVH <IHVH@inbox.ru>
2023-04-07 13:44:07 +0300

    example register_next_step_handler

--- src/functions/example_bot_function.py
+++ src/functions/example_bot_function.py
@@ -12,7 +12,7 @@ class ExampleBotFunction(BotFunctionABC):
 
         @bot.message_handler(commands=commands)
         def example_message_hendler(message: types.Message):
-            msg = f'Ваш запрос обработан в ExampleBotFunction! EXAMPLETOKEN={self.get_example_token()}'
+            msg = f"Ваш запрос обработан в ExampleBotFunction! \nUSER ID = {message.from_user.id} \nEXAMPLETOKEN = {self.get_example_token()}"
             print(msg)
             bot.send_message(text=msg, chat_id=message.chat.id, reply_markup=self.gen_markup())
 
@@ -26,17 +26,32 @@ class ExampleBotFunction(BotFunctionABC):
                     bot.answer_callback_query(call.id, "Ответ ДА!")
                 case ('cb_no'):
                     bot.answer_callback_query(call.id, "Ответ НЕТ!")
+                case ('force_reply'):
+                    force_reply = types.ForceReply(selective=False)
+                    bot.send_message(call.message.chat.id, "Отправьте текст для обработки в process_next_step", reply_markup=force_reply)
+                    bot.register_next_step_handler(call.message, self.process_next_step)
                 case _:
                     bot.answer_callback_query(call.id, call.data)
 
-
     def get_example_token(self):
-        token = os.environ["EXAMPLETOKEN"]
+        token = os.environ.get("EXAMPLETOKEN")
         return token
 
     def gen_markup(self):
         markup = types.InlineKeyboardMarkup()
         markup.row_width = 2
         markup.add(types.InlineKeyboardButton("Да", callback_data=self.example_keyboard_factory.new(t_key_button="cb_yes")),
-                   types.InlineKeyboardButton("Нет", callback_data=self.example_keyboard_factory.new(t_key_button="cb_no")))
+                   types.InlineKeyboardButton("Нет", callback_data=self.example_keyboard_factory.new(t_key_button="cb_no")),
+                   types.InlineKeyboardButton("ForceReply", callback_data=self.example_keyboard_factory.new(t_key_button="force_reply")), )
         return markup
+
+    def process_next_step(self, message):
+        try:
+            chat_id = message.chat.id
+            txt = message.text
+            if txt != "exit":
+                force_reply = types.ForceReply(selective=False)
+                msg = self.bot.send_message(message.chat.id, f"text = {txt}; chat.id = {chat_id}; \n Для выхода из диалога введите - exit", reply_markup=force_reply)
+                self.bot.register_next_step_handler(msg, self.process_next_step)
+        except Exception as e:
+            self.bot.reply_to(message, f"Exception - {e}")
