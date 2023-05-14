0575355ef934788eb52cd5d071bd0ede76cd4246
Гербарий <91408666+Lizapopa40@users.noreply.github.com>
2023-05-14 20:44:03 +0300

    Create AnimCitata.py
    Цитаты из "Аниме"
    Шувалов Е.А.

---
+++ src/functions/AnimCitata.py
@@ -0,0 +1,14 @@
+import requests
+import telebot
+from telebot import types
+from bot_func_abc import BotFunctionABC
+from typing import List
+
+class CitataGenerator(BotFunctionABC):
+    def set_handlers(self, bot: telebot.TeleBot, commands: List[str]):
+        self.bot = bot
+        @bot.message_handler(commands=commands)
+        def generate_and_answer(message: types.Message):
+            res = requests.get('https://animechan.vercel.app/api/random')
+            res_json = res.json()
+            content = res_json["results"][0]
