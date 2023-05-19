0782cdfe454ec7c4a3e3c3cd82a315627b8a3205
ino943 <133962932+ino943@users.noreply.github.com>
2023-05-19 16:02:37 +0300

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -9,20 +9,24 @@ class GetBotHostIP(BotFunctionABC):
         self.bot = bot
         @bot.message_handler(commands=commands)
         def generate_and_answer(message: types.Message):
-            res_ip = requests.get('https://api.ipify.org/?format=json')
-            res_json = res_ip.json()
-            bot_host_ip = res_json["ip"]
 
-            res_loc = requests.get('https://ipinfo.io/' + bot_host_ip + '/geo')
+            input_str = message.text.split()
+            ip = input_str[-1]
+
+            res_loc = requests.get('https://ipinfo.io/' + ip + '/geo')
             res_json = res_loc.json()
-            bot_loc_info = {
-                        "IP-адрес хоста бота: " + res_json["ip"] + ";\n" +
-                        "Временной пояс: " + res_json["timezone"] + ";\n" +
-                        "Страна: " + res_json["country"] + ";\n" +
-                        "Регион: " + res_json["region"] + ";\n" +
-                        "Город: " + res_json["city"] + ";\n" +
-                        "Провайдер: " + res_json["org"] + ";\n" +
-                        "GEO: " + res_json["loc"] + ";\n"
-                        }
+
+            if "status" in res_json:
+                bot_loc_info = {"Неверно введённый IP-адрес!"}
+            else:
+                bot_loc_info = {
+                            "IP-адрес хоста бота: " + res_json["ip"] + ";\n" +
+                            "Временной пояс: " + res_json["timezone"] + ";\n" +
+                            "Страна: " + res_json["country"] + ";\n" +
+                            "Регион: " + res_json["region"] + ";\n" +
+                            "Город: " + res_json["city"] + ";\n" +
+                            "Провайдер: " + res_json["org"] + ";\n" +
+                            "GEO: " + res_json["loc"] + ";\n"
+                            }
 
             bot.send_message(message.chat.id, bot_loc_info)
