15a9e698c4a51780fa0ed10f10d73a1e56c56f27
IHVH <IHVH@inbox.ru>
2023-05-20 14:10:11 +0300

    fix quote

--- src/functions/quotes.py
+++ src/functions/quotes.py
@@ -1,12 +1,8 @@
 from typing import List
-
 import telebot
 import requests
-from bs4 import BeautifulSoup
 from telebot.callback_data import CallbackData
-
 from bot_func_abc import BotFunctionABC
-
 
 class Quotes(BotFunctionABC):
     def set_handlers(self, bot: telebot.TeleBot, commands: List[str]):
@@ -15,10 +11,5 @@ class Quotes(BotFunctionABC):
 
         @bot.message_handler(commands=commands)
         def send_welcome(message):
-            self.bot.reply_to(message,
-                              'Привет! Я бот для получения цитат и афоризмов из русских источников. Введите /quote, чтобы получить случайную цитату.')
-
             response = requests.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=ru')
-            soup = BeautifulSoup(response.text, 'html.parser')
-            quote = soup.get_text()
-            self.bot.reply_to(message, quote)
+            self.bot.send_message(message.chat.id, response.text)
