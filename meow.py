c388be9230f082b4feac797be89e8feca59484db
123bina <134422455+123bina@users.noreply.github.com>
2023-05-23 22:11:17 +0300

    Update meow.py

--- meow.py
+++ meow.py
@@ -4,26 +4,15 @@ from typing import List
 from bot_func_abc import BotFunctionABC
 
 class meowfact(BotFunctionABC):
-    def __init__(self):
-        self.random_fact_data = "random_cat_fact"
-    
     def set_handlers(self, bot: telebot.TeleBot, commands: List[str]):
         self.bot = bot
             
         @bot.message_handler(commands=commands)
         def cat_fact_handler(message):
             chat_id = message.chat.id
-            
             response = requests.get("https://catfact.ninja/fact")
             data = response.json()
             fact = data['fact']
             message_text = "💓Meow fact💓\n\n" + fact 
             bot.send_message(chat_id=chat_id, text=message_text)
     
-    def send_random_cat_fact(self, chat_id: int):
-        response = requests.get("https://catfact.ninja/fact")
-        data = response.json()
-        fact = data['fact']
-
-        markup = self.get_buttons_markup()
-        self.bot.send_message(chat_id=chat_id, text=fact, reply_markup=markup)
