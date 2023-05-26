25b9e2b606d33763f167ff394f88d38e907cc243
Cash3mod3l <134133773+Tamoe001@users.noreply.github.com>
2023-05-26 22:12:06 +0300

    рефакторинг

--- src/functions/currencyConversion.py
+++ src/functions/currencyConversion.py
@@ -10,16 +10,16 @@ class CurrencyConversion(BotFunctionABC):
         amount = 0
         @bot.message_handler(commands=commands)
         def start(message):
-            bot.send_message(message.chat.id, 'Привет,введите сумму')
-            bot.register_next_step_handler(message, summa)
+            self.bot.send_message(message.chat.id, 'Привет,введите сумму')
+            self.bot.register_next_step_handler(message, summa)
 
         def summa(message):
             global amount
             try:
                 amount = int(message.text.strip())
             except ValueError:
-                bot.send_message(message.chat.id, 'Неверный формат,введите сумму')
-                bot.register_next_step_handler(message, summa)
+                self.bot.send_message(message.chat.id, 'Неверный формат,введите сумму')
+                self.bot.register_next_step_handler(message, summa)
                 return
             if amount > 0:
                 markup = types.InlineKeyboardMarkup(row_width=2)
@@ -28,28 +28,28 @@ class CurrencyConversion(BotFunctionABC):
                 btn3 = types.InlineKeyboardButton('USD/GBP', callback_data='usd/gbp')
                 btn4 = types.InlineKeyboardButton('Другое значение', callback_data='else')
                 markup.add(btn1, btn2, btn3, btn4)
-                bot.send_message(message.chat.id, 'Выбирите пару валют', reply_markup=markup)
+                self.bot.send_message(message.chat.id, 'Выбирите пару валют', reply_markup=markup)
             else:
-                bot.send_message(message.chat.id, 'Чило должно быть больше 0 ,введите сумму')
-                bot.register_next_step_handler(message, summa)
+                self.bot.send_message(message.chat.id, 'Чило должно быть больше 0 ,введите сумму')
+                self.bot.register_next_step_handler(message, summa)
 
-        @bot.callback_query_handler(func=lambda call: True)
+
         def callback(call, currency=None):
             if call.data != 'else':
                 values = call.data.upper().split('/')
                 res = currency.convert(amount, values[0], values[1])
-                bot.send_message(call.message.chat.id, f'Получается: {round(res, 2)}.Можете заново вписать сумму')
-                bot.register_next_step_handler(call.message, summa)
+                self.bot.send_message(call.message.chat.id, f'Получается: {round(res, 2)}.Можете заново вписать сумму')
+                self.bot.register_next_step_handler(call.message, summa)
             else:
-                bot.send_message(call.message.chat.id, 'Введите пару значений через слэш')
-                bot.register_next_step_handler(call.message, mycurrency)
+                self.bot.send_message(call.message.chat.id, 'Введите пару значений через слэш')
+                self.bot.register_next_step_handler(call.message, mycurrency)
 
         def mycurrency(message, currency=None):
             try:
                 values = message.text.upper().split('/')
                 res = currency.convert(amount, values[0], values[1])
-                bot.send_message(message.chat.id, f'Получается: {round(res, 2)}  .Можете заново вписать сумму')
-                bot.register_next_step_handler(call.message, summa)
+                self.bot.send_message(message.chat.id, f'Получается: {round(res, 2)}  .Можете заново вписать сумму')
+                self.bot.register_next_step_handler(call.message, summa)
             except Exception:
-                bot.send_message(message.chat.id, 'Что-то не так. Впишите сумму')
-                bot.register_next_step_handler(message, summa)
+                self.bot.send_message(message.chat.id, 'Что-то не так. Впишите сумму')
+                self.bot.register_next_step_handler(message, summa)
